0a468117db89e493be28350ce14c67c8e8e63be2
Maelos <fox2jm@jmu.edu>
2018-05-16 17:27:22 -0400

    Cool stuff.  Tested the functions in FreeNAS for IPMI tool to confirmed they worked, found that only the file with the password needed to be passed.  I also found that the received information from the command Go functions needed to be converted for a slice of bytes to bits and then to floats.

--- freenas_exporter.go
+++ freenas_exporter.go
@@ -1,8 +1,11 @@
 package main
 
 import (
-	"io/ioutil" //for reading the password for IPMI
-	"os/exec"   //for using external processes - IPMI and smartctl calls
+	"encoding/binary"
+	"math"
+	//for reading the password for IPMI
+	"fmt"
+	"os/exec" //for using external processes - IPMI and smartctl calls
 
 	"github.com/prometheus/client_golang/prometheus"
 )
@@ -17,32 +20,58 @@ func getCPUtemps() (out []float64) {
 
 	// IPMI Variables/Settings
 	//useIPMI := true //I want to use IPMI, use what you like
-	ipmiHost := "192.168.1.64" // IP address or DNS-resolvable hostname of IPMI server:
-	ipmiUser := "root"         // IPMI username
+	ipmiHost := "192.168.1.60" // IP address or DNS-resolvable hostname of IPMI server:
+	ipmiUser := "ADMIN"        // IPMI username
 	// IPMI password file. This is a file containing the IPMI user's password
 	// on a single line and should have 0600 permissions:
-	ipmiPWfromFile, _ := ioutil.ReadFile("/root/ipmi_password") //needs to find the file at location and read the line to the variable
-	ipmiPW := string(ipmiPWfromFile)
+	ipmiPWFile := "/root/ipmi_password" //just the file location
 
 	//define the command to get the number of CPUs and then use it
-	numCPUCmd := exec.Command("/usr/local/bin/ipmitool", " -I lanplus -H ", ipmiHost, " -U ", ipmiUser, " -f ", ipmiPW, " sdr elist all | grep -c -i 'cpu.*temp")
-	numCPUSoB, _ := numCPUCmd.Output() //returns a slice of bytes and an error
-	numCPU := int(numCPUSoB[0])        //converts the first and hopefully only value of slice of bytes into an int
+	numCPUCmd := exec.Command("/usr/local/bin/ipmitool", " -I lanplus -H ", ipmiHost, " -U ", ipmiUser, " -f ", ipmiPWFile, " sdr elist all | grep -c -i 'cpu.*temp'")
+	numCPUBytes, _ := numCPUCmd.Output() //returns a slice of bytes and an error
+	numCPUBits := binary.LittleEndian.Uint64(numCPUBytes)
+	numCPUFloat := math.Float64frombits(numCPUBits)
+	numCPU := int(numCPUFloat) //converts the first and hopefully only value of slice of bytes into an int
+
+	fmt.Println(numCPU) //error checking
 
 	//go through each CPU and get the temperature
 	if numCPU == 1 {
 		//define the command used to get the CPU temperature
-		tempCmd := exec.Command("/usr/local/bin/ipmitool", " -I lanplus -H ", ipmiHost, " -U ", ipmiUser, " -f ", ipmiPW, " sdr elist all | grep 'CPU Temp' | awk '{print $10}'")
-		temp, _ := tempCmd.Output()
-		out = append(out, float64(temp[0]))
+		tempCmd := exec.Command("/usr/local/bin/ipmitool", " -I lanplus -H ", ipmiHost, " -U ", ipmiUser, " -f ", ipmiPWFile, " sdr elist all | grep 'CPU Temp' | awk '{print $10}'")
+		fmt.Println(tempCmd) //error checking
+
+		tempBytes, _ := tempCmd.Output()
+		fmt.Println("Printing tempBytes", tempBytes) //error checking
+
+		tempBits := binary.LittleEndian.Uint64(tempBytes)
+		fmt.Println("Printing tempBits", tempBits) //error checking
+
+		tempFloat := math.Float64frombits(tempBits)
+		fmt.Println("Printing tempFloat", tempFloat) //error checking
+
+		out = append(out, tempFloat)
+		fmt.Println("Printing out", out) //error checking
 	} else {
-		for i := 0; i < numCPU; i++ {
-			tempCmd := exec.Command("/usr/local/bin/ipmitool", " -I lanplus -H ", ipmiHost, " -U ", ipmiUser, " -f ", ipmiPW, " sdr elist all | grep 'CPU", string(i), " Temp' | awk '{print $10}'")
-			temp, _ := tempCmd.Output()
-			out = append(out, float64(temp[0]))
+		for i := numCPU; i > 0; i-- {
+			tempCmd := exec.Command("/usr/local/bin/ipmitool", " -I lanplus -H ", ipmiHost, " -U ", ipmiUser, " -f ", ipmiPWFile, " sdr elist all | grep 'CPU", string(i), " Temp' | awk '{print $10}'")
+			fmt.Println(tempCmd) //error checking
+
+			tempBytes, _ := tempCmd.Output()
+			fmt.Println("Printing tempBytes", tempBytes) //error checking
+
+			tempBits := binary.LittleEndian.Uint64(tempBytes)
+			fmt.Println("Printing tempBits", tempBits) //error checking
+
+			tempFloat := math.Float64frombits(tempBits)
+			fmt.Println("Printing tempFloat", tempFloat) //error checking
+
+			out = append(out, tempFloat)
+			fmt.Println("Printing out", out) //error checking
 		}
 	}
-	return out // returns the slice of float64s
+	fmt.Println(out) //error checking
+	return out       // returns the slice of float64s
 }
 
 //You must create a constructor for you collector that
